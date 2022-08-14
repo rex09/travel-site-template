396cec97194312a1ecd93678b06b363bca85dfc3
rex09 <raj35940@gmail.com>
2022-08-14 15:49:00 +0530

    updated file

---
+++ app/assets/scripts/app.js
@@ -0,0 +1,2 @@
+import '../styles/styles.css'
+alert("Hello, sky is blue.");
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+    entry: './app/assets/scripts/app.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['css-loader']
+            }
+        ]
+    }
+}
